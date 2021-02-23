d60af9fe6dc06f8b7ceb7f74b750f28850bb1189
DevClancy <70335809+DevClancy@users.noreply.github.com>
2021-02-23 23:51:13 +0300

    Update UCFr.py

--- UCFr.py
+++ UCFr.py
@@ -39,6 +39,15 @@ ip = input(Fore.CYAN + "\nEnter IP: ")
 port = input(Fore.CYAN + "Enter PORT: ")
 threads = int(input(Fore.CYAN + "Enter THREADS: "))
 
+def info():
+	while True():
+		try:
+			os.system("cls")
+			print(Fore.RED + "Sent: " + sent + Fore.RED + "Errors: " + error)
+		except:
+			os.system("clear")
+			print(Fore.RED + "Sent: " + sent + Fore.RED + "Errors: " + error)
+
 def attack():
 	with open(r"socks5.txt", "r") as file:
 		for line in file:
